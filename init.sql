46e6e77994f1d22994f50a66c4bd35c8c6fc9029
Ubuntu <ubuntu@ip-172-31-70-239.ec2.internal>
2026-04-07 12:00:22 +0000

    Add Tea to task list

--- init.sql
+++ init.sql
@@ -1,2 +1,4 @@
 CREATE TABLE IF NOT EXISTS tasks (id SERIAL PRIMARY KEY, name TEXT, status TEXT);
-INSERT INTO tasks (name, status) VALUES ('Task 1', 'pending'), ('Task 2', 'completed');
+INSERT INTO tasks (name, status) VALUES ('Task 1', 'pending'), ('Task 2', 'completed')
+INSERT INTO tasks (name, status) VALUES ('Tea', 'pending');
+
