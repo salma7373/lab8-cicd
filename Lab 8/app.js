1e86e21bb7a0c54f2fca79d956d11fc0fa159725
Salma Abdelfattah <salma.essamatya102@gmail.com>
2026-04-07 11:46:08 +0200

    Complete Task 1: Connect Node.js to Postgres

---
+++ Lab 8/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { Pool } = require('pg'); 
+const app = express();
+const port = 3000;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM tasks'); 
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error connecting to DB");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
