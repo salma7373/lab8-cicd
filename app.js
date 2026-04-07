3e986aade9e49891de4405b8c8769ec6c7340047
Ubuntu <ubuntu@ip-172-31-70-239.ec2.internal>
2026-04-07 14:47:09 +0000

    Fix database environment variables

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const port = 3000;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
-
 app.get('/tasks', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM tasks'); 
